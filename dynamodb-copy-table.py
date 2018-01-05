355692853cc6dc5483188db1b6d46e6c92db9cdd
Jon Austen <djangofan@gmail.com>
2018-01-04 18:18:45 -0800

    Update dynamodb-copy-table.py

--- dynamodb-copy-table.py
+++ dynamodb-copy-table.py
@@ -47,7 +47,8 @@ try:
     logs = Table(src_table, connection=ddbc)
     table_struct = logs.describe()
 except:
-    print "ERROR: Failure reading table %s" % src_table
+    #print "ERROR: Failure reading table %s" % src_table
+    print 'ERROR: Failure reading table {0} from region {1}'.format(src_table, tableRegion)
     sys.exit(1)
 
 print '*** Reading key schema from %s table' % src_table
